0d8e0becf90f293ab01d0a013ed53c44c2ff185e
valentin <vb.brebionpro@gmail.com>
2024-02-13 09:09:05 +0100

    modal OK

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+ import ModalView from '../src/components/slots/TheModal.vue'
+ const showModalHello = ref(false)
+ const showModalBy = ref(false)
+</script>
+
+<template>
+  <ModalView :showModal="showModalHello" @close="showModalHello = false">
+    <template v-slot:title>Hello</template>
+    <p class="py-4">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, rem nihil. Ullam
+    placeat natus aliquam, molestias cumque, ipsam praesentium sint culpa animi voluptatibus
+    itaque libero debitis aliquid laudantium nam quam!
+  </p>
+  </ModalView>
+  <ModalView :showModal="showModalBy" @close="showModalBy = false">
+    <template v-slot:title>Aurevoir</template>
+    <img src="./assets/bye.gif" alt="Cat say bye" />
+  </ModalView>
+  <button class="btn btn-primary" @click="showModalHello = true">Dites bonjour 👋</button>
+  <button class="btn btn-primary" @click="showModalBy = true">Dites aurevoir 👋</button>
+ 
+ 
+</template>
---
+++ src/components/slots/TheModal.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="modal" :class="{ 'modal-open': props.showModal }">
+    <div class="modal-box">
+      <form method="dialog">
+        <button
+          class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+          @click="emit('close')"
+        >
+          ✕
+        </button>-
+      </form>
+      <h3 class="font-bold text-lg"><slot name="title"></slot></h3>
+
+      <p class="py-4">
+        <slot></slot>
+      </p>
+    </div>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+    showModal : {
+        type: Boolean
+    }
+})
+const emit = defineEmits(['close'])
+</script>
