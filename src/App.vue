a0d8b9756d9e0c23373f2c3d7574dd9a8ee08b43
valentin <vb.brebionpro@gmail.com>
2024-02-19 22:13:04 +0100

    display message and detail when click on detail button

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,77 @@
+<template>
+  <div>
+    <ModalView :showModal="openDetailsModal" @close="toggleDetailsModal">
+      <template #title>Utilisateur</template>
+      <p v-if="selectedPeople">{{ selectedPeople.name }}</p>
+      <template #actions>
+        <button class="btn btn-ghost" @click="toggleDetailsModal">Fermer</button>
+      </template>
+    </ModalView>
+
+    <ModalView :showModal="openMessageModal" @close="toggleMessageModal">
+      <template #title>Mes messages</template>
+      {{ selectedPeople?.message }}
+    </ModalView>
+
+    <PeopleTable :items="people" :columns="columns">
+      <template #isActive="{ people }">
+        <div
+          class="badge badge-md"
+          :class="{ 'badge-success': people.isActive, 'badge-error': !people.isActive }"
+        ></div>
+      </template>
+      <template #actions="{ item }">
+        <div class="flex items-center gap-2">
+          <button class="btn btn-ghost btn-sm" @click="toggleDetailsModal(item)">Details</button>
+          <button class="btn btn-ghost btn-square btn-sm" @click="toggleMessageModal(item)">
+            <IconMessage></IconMessage>
+          </button>
+        </div>
+      </template>
+    </PeopleTable>
+  </div>
+</template>
+
 <script setup>
-import { ref , computed} from 'vue'
- import ModalView from '../src/components/slots/TheModal.vue'
- import PeopleTable from '../src/components/PeopleTable.vue'
- import getPeople from '@/services/api.service.js'
- const showModalHello = ref(false)
- const showModalBy = ref(false)
- const people = ref([])
- const openDetailsModal = ref(false)
-const openMessageModal = ref(false)
-const selectedPeopleId = ref()
-const selectedPeople = computed(() =>
-  people.value.find((people) => people.guid === selectedPeopleId.value)
-)
+import { ref, computed } from 'vue';
+import ModalView from '../src/components/slots/TheModal.vue';
+import PeopleTable from '../src/components/PeopleTable.vue';
+import IconMessage from '@/components/icons/IconMessage.vue';
+import getPeople from '@/services/api.service.js';
 
-function toggleDetailsModal(id) {
-  openDetailsModal.value = !openDetailsModal.value
-  selectedPeopleId.value = id
-}
+const people = ref([]);
+const openDetailsModal = ref(false);
+const openMessageModal = ref(false);
+const selectedPeopleId = ref(null);
 
-function toggleMessageModal(id) {
-  openMessageModal.value = !openMessageModal.value
-  selectedPeopleId.value = id
-}
-
-
- async function fetchPeople() {
-        try {
-            const response = await getPeople();
-            people.value = response.data;
-            console.log(people)
-        } catch (error) {
-            console.error('Erreur lors de la récupération des peoples:', error)
-        }
-    }
-    const columns = [
-  {
-    "name": "name",
-    "label": "Nom"
-  },
-  {
-    "name": "isActive",
-    "label": "Statut"
-  },
-  {
-    "name": "company",
-    "label": "Entreprise"
+async function fetchPeople() {
+  try {
+    const response = await getPeople();
+    people.value = response.data;
+  } catch (error) {
+    console.error('Erreur lors de la récupération des personnes:', error);
   }
+}
+
+function toggleDetailsModal(item) {
+  openDetailsModal.value = !openDetailsModal.value;
+  selectedPeopleId.value = item;
+}
+
+function toggleMessageModal(item) {
+  openMessageModal.value = !openMessageModal.value;
+  selectedPeopleId.value = item;
+}
+const selectedPeople = computed(() =>
+  people.value.find((person) => person.guid === selectedPeopleId.value)
+);
+
+const columns = [
+  { "name": "name", "label": "Nom" },
+  { "name": "isActive", "label": "Statut" },
+  { "name": "company", "label": "Entreprise" },
+  { "name": "id", "label": "guid" }
 ];
 
-  fetchPeople()
+fetchPeople();
 </script>
-<template>
-  <PeopleTable :items="people" :columns="columns">
-    <template #isActive="{ people }">
-        <div
-      class="badge badge-md"
-      :class="{ 'badge-success': people.isActive, 'badge-error': !people.isActive }"
-    ></div>
-    </template>
-    <template #actions="{ id }">
-      <div class="flex items-center gap-2">
-    <button class="btn btn-ghost btn-sm" @click="toggleDetailsModal(id)">Details</button>
-    <button class="btn btn-ghost btn-square btn-sm" @click="toggleMessageModal(id)">
-      <IconMessage></IconMessage>
-    </button>
-  </div>
-  </template>
-  </PeopleTable>
-
-  <ModalView :showModal="showModalHello" @close="showModalHello = false">
-    <template v-slot:title>Hello</template>
-    <p class="py-4">
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, rem nihil. Ullam
-    placeat natus aliquam, molestias cumque, ipsam praesentium sint culpa animi voluptatibus
-    itaque libero debitis aliquid laudantium nam quam!
-  </p>
-  </ModalView>
-  <ModalView :showModal="showModalBy" @close="showModalBy = false">
-    <template v-slot:title>Aurevoir</template>
-    <img src="./assets/bye.gif" alt="Cat say bye" />
-  </ModalView>
-  <button class="btn btn-primary" @click="showModalHello = true">Dites bonjour 👋</button>
-  <button class="btn btn-primary" @click="showModalBy = true">Dites aurevoir 👋</button>
-
-</template>
--- src/components/PeopleTable.vue
+++ src/components/PeopleTable.vue
@@ -1,34 +1,32 @@
 <template>
-    <div class="overflow-x-auto">
-      <table class="table">
-        <!-- head -->
-        <thead>
-            <tr>
-                <th v-for="column in props.columns" :key="column.name">
-                <slot :name="`header-${column.name}`">
-                    {{ column.label }}
-                </slot>
-                </th>
-                    <!-- Actions column-->
-                    <th>
-                        <slot name="actions" :id="item.guid"></slot>
-                    </th>
-            </tr>
-        </thead>
-        <!-- body -->
-        <tbody>
-            <tr v-for="item in props.items" :key="item.id">
-                <td v-for="column in props.columns" :key="column.name">
-                        <slot :name="column.name" :people="item">
-                            {{ item[column.name] }}
-                        </slot>
-                </td>
-            </tr>
-        </tbody>
-      </table>
-    </div>
-  
-  </template>
+  <div class="overflow-x-auto">
+    <table class="table">
+      <!-- head -->
+      <thead>
+        <tr>
+          <th v-for="column in props.columns" :key="column.name">
+            <slot :name="`header-${column.name}`">
+              {{ column.label }}
+            </slot>
+          </th>
+        </tr>
+      </thead>
+      <!-- body -->
+      <tbody>
+        <tr v-for="item in props.items" :key="item.id">
+          <td v-for="column in props.columns" :key="column.name">
+            <slot :name="column.name" :people="item">
+              {{ item[column.name] }}
+            </slot>
+          </td>
+          <td>
+            <slot name="actions" :item="item.guid"></slot>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
   
   <script setup>
   import { defineProps } from 'vue';
@@ -45,11 +43,4 @@
   });
   </script>
   
-  <style scoped>
-  .displaytab {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-  }
-  </style>
   
